a57f3366c3c82598c658ab30bda1c69d9952e11a
doctorhh <hans.harvey@h2hteam.com>
2017-07-06 14:12:29 +0200

    Removed tidyverse library
    tidyverse was causing trouble loading.
    Added XTS and other package for processing and developing function or dataframe?

--- Oura/oura_output_tidydata.R
+++ Oura/oura_output_tidydata.R
@@ -5,7 +5,22 @@ setwd('~/GitHub/curiouspal/Oura') # windows Laptop
 library(jsonlite)
 library(dplyr)
 library(ggplot2)
-library(tidyverse)
+library(purrr)
+library(parsedate)
+library(stringr)
+library(zoo)
+library(xts)
 
 #Loading JSON from Oura output
 file <- fromJSON('sleep_oura.json')
+
+#Date parsing from ISO to "POSIXct" "POSIXt" 
+s_date <- file$sleep$bedtime_start[1]
+e_date <- file$sleep$bedtime_end[1]
+#converting to POSIXct with Lubridate
+s_pdate <- ymd_hms(s_date, tz = "CET")
+e_pdate <- ymd_hms(e_date, tz = "CET")
+#calculating 5min interval from Sleep Start to Sleep end
+#This can/could/will be used for bulding a timeseries (zoo)
+time_spam <- seq.POSIXt(s_pdate,e_pdate,by='5 min')
+
